d2d6fe71c0a95addf981af315ece1e340ae1e1c8
Radhebhaitg <rlmeena2024@gmail.com>
2025-03-23 17:23:53 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,9 +1,9 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @gopalsharmatg
+# Subscribe YouTube Channel For Amazing Bot
+# Ask Doubt on telegram @gopalsharmatg
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", "22182189"))
-API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
+API_ID = int(environ.get("API_ID", "14012535"))
+API_HASH = environ.get("API_HASH", "19b6b514b108611a6a0b51325fe53999")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
